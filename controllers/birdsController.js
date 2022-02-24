bbd15b29a9a00091b813406f532ec67587810067
anthony <adkowalkowski@gmail.com>
2022-02-23 20:14:03 -0600

    modified structure of birds array and added post, get, put, and delete routes inside birdController

--- controllers/birdsController.js
+++ controllers/birdsController.js
@@ -9,4 +9,25 @@ router.get("/", (req, res) => {
     res.send(birds);
 })
 
+router.post("/", (req, res) => {
+    console.log("post route");
+    console.log(req.body);
+    birds.push(req.body);
+    res.send("received post request");
+})
+
+router.get("/:id", (req, res) => {
+    res.send(birds[req.params.id - 1]);
+})
+
+router.delete("/:id", (req, res) => {
+    birds.splice(req.params.id - 1, 1);
+    res.send("deleted bird");
+})
+
+router.put("/:id", (req, res) => {
+    birds[req.params.id] = req.body
+    res.send(birds[req.params.id]);
+})
+
 module.exports = router; 

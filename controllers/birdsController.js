049e57b8f58fe4b6c01065c642ba32bd3ba74549
anthony <adkowalkowski@gmail.com>
2022-02-24 10:58:54 -0600

    final commit for homework submission

--- controllers/birdsController.js
+++ controllers/birdsController.js
@@ -27,7 +27,7 @@ router.delete("/:id", (req, res) => {
 
 router.put("/:id", (req, res) => {
     birds[req.params.id] = req.body
-    res.send(birds[req.params.id]);
+    res.send(birds[req.params.id - 1]);
 })
 
 module.exports = router; 
